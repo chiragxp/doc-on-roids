073163f5e6a17569f29572e49da3d3987b8f2956
Chirag Parmar <95179516+chiragxp@users.noreply.github.com>
2025-03-16 02:47:48 -0400

    Implemented collaborative room and multiuser access to single document

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,9 +1,24 @@
 import CollaborativeRoom from "@/components/CollaborativeRoom";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { getDocument } from "@/lib/actions/room.actions";
 
-const Document = () => {
+const Document = async ({ params: { id } }: SearchParamProps) => {
+  const clerkUser = await currentUser();
+  if (!clerkUser) redirect("/sign-in");
+
+  const room = await getDocument({
+    roomId: id,
+    userId: clerkUser.emailAddresses[0].emailAddress,
+  });
+
+  if (!room) redirect("/");
+
+  // TO DO: Assess the permissions of the user to access the document
+
   return (
     <main className="flex w-full flex-col items-center">
-      <CollaborativeRoom />
+      <CollaborativeRoom roomId={id} roomMetadata={room.metadata} />
     </main>
   );
 };
